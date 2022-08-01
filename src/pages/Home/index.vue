cb3bbae2e9d728dc2c9f61268da1a3d0e4a48d98
GISTWJ <3238670575@qq.com>
2022-08-01 16:28:06 +0800

    Create index.vue

---
+++ src/pages/Home/index.vue
@@ -0,0 +1,23 @@
+<template> 
+<div class="big"> 
+    <TypeNav></TypeNav> 
+    <Contents></Contents>
+</div>
+</template>
+
+<script> 
+//引入组件  
+import  Contents from '../Home/Contents'
+export default { 
+    name :'', 
+    components:{ 
+        Contents,
+    },
+}
+</script>
+
+<style> 
+.big{ 
+    display: flex;
+}
+</style>
