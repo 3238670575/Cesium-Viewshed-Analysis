6efcb1fda4c36f090c9ae0799144470d72c7dbd7
GISTWJ <3238670575@qq.com>
2022-08-01 16:28:29 +0800

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+   <Header></Header>  
+   <!-- 路由组件出口 --> 
+   <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header/index.vue'
+
+export default {
+  name: 'App',
+  components: { 
+    Header, 
+  }
+}
+</script>
+
+<style> 
+html , body { 
+  width: 100%; 
+  height: 100%; 
+  margin: 0; 
+  padding: 0; 
+  overflow: hidden; 
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
